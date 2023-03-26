69d9ff26b7278e16bf3ceaee45ee095df4cfe12f
Oleksandr Korobko <korobkoalexander5901@gmail.com>
2023-03-26 16:42:28 +0300

    add java docs

--- src/main/kotlin/com/github/chatgptassistant/assistantback/usecase/ChatUseCase.kt
+++ src/main/kotlin/com/github/chatgptassistant/assistantback/usecase/ChatUseCase.kt
@@ -4,10 +4,50 @@ import com.github.chatgptassistant.assistantback.domain.Chat
 import org.springframework.data.domain.Page
 import java.util.*
 
+/**
+ * Chat use case.
+ */
 interface ChatUseCase {
+  /**
+   * Create a new chat.
+   *
+   * @param userId user id
+   * @param title chat title
+   * @return created chat
+   */
   fun createChat(userId: UUID, title: String): Chat
+
+  /**
+   * Delete a chat.
+   *
+   * @param userId user id
+   * @param chatId chat id
+   */
   fun deleteChat(userId: UUID, chatId: UUID)
+
+  /**
+   * Delete all chats for a user.
+   *
+   * @param userId user id
+   */
   fun deleteAllChats(userId: UUID)
+
+  /**
+   * Fetch all chats for a user.
+   *
+   * @param userId user id
+   * @param page page number
+   * @param size page size
+   * @return page of chats
+   */
   fun fetchAllChats(userId: UUID, page: Int, size: Int): Page<Chat>
+
+  /**
+   * Update chat title.
+   *
+   * @param userId user id
+   * @param chatId chat id
+   * @return chat
+   */
   fun updateChatTitle(userId: UUID, chatId: UUID, newTitle: String): Chat
 }
--- src/main/kotlin/com/github/chatgptassistant/assistantback/usecase/MessageUseCase.kt
+++ src/main/kotlin/com/github/chatgptassistant/assistantback/usecase/MessageUseCase.kt
@@ -5,15 +5,56 @@ import com.github.chatgptassistant.assistantback.domain.Content
 import com.github.chatgptassistant.assistantback.domain.Message
 import java.util.*
 
+/**
+ * Message use case.
+ */
 interface MessageUseCase {
+  /**
+   * Fetch all messages in a chat.
+   *
+   * @param userId user id
+   * @param chatId chat id
+   * @param currentNode current node id. If null, take the chat's current node.
+   * @param upperLimit upper limit of messages
+   * @param lowerLimit lower limit of messages
+   * @return list of chat nodes
+   */
   fun fetchAllMessages(userId: UUID, chatId: UUID, currentNode: UUID?, upperLimit: Int, lowerLimit: Int): List<ChatNode>
+
+  /**
+   * Post a message to the chat and generate AI model response.
+   *
+   * @param userId user id
+   * @param chatId chat id
+   * @param content message content
+   * @return response message
+   */
   fun postMessageAndGenerateResponse(userId: UUID, chatId: UUID, content: Content): Message
+
+  /**
+   * Edit a message and regenerate AI model response.
+   *
+   * @param userId user id
+   * @param chatId chat id
+   * @param messageId message id
+   * @param newContent new message content
+   * @return pair of edited message and response message
+   */
   fun editMessageAndRegenerateResponse(
     userId: UUID,
     chatId: UUID,
     messageId: UUID,
     newContent: Content
   ): Pair<Message, Message>
+
+  /**
+   * Regenerate AI model response for the message.
+   *
+   * @param userId user id
+   * @param chatId chat id
+   * @param messageId message id
+   * @return response message
+   */
   fun regenerateResponse(userId: UUID, chatId: UUID, messageId: UUID): Message
 
 }
