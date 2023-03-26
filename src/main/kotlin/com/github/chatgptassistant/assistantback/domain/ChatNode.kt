8012db2a2476d0663f10951da9a6e3ab6304cd83
Oleksandr Korobko <korobkoalexander5901@gmail.com>
2023-03-26 21:01:07 +0300

    add Index

--- src/main/kotlin/com/github/chatgptassistant/assistantback/domain/ChatNode.kt
+++ src/main/kotlin/com/github/chatgptassistant/assistantback/domain/ChatNode.kt
@@ -1,6 +1,7 @@
 package com.github.chatgptassistant.assistantback.domain
 
 import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.index.Indexed
 import java.util.*
 
 //TODO: add token's count to the each Node, so we can know how many tokens we have in the subtree
@@ -10,6 +11,7 @@ data class ChatNode(
   val chatId: UUID,
   val parent: UUID? = null,
   val children: List<UUID>,
+  @Indexed
   val ancestors: List<UUID>,
   val message: Message
 )
